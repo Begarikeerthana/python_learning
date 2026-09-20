7fa96b5db25ec52b80b9cf8288c80e2d0c68fd8e
Keerthana <begarikeerthana05@gmail.com>
2026-09-20 21:59:22 +0530

    day2 task completed

--- day03.py
+++ day03.py
@@ -84,121 +84,121 @@ o = tuple({4,5,6})
 p = tuple({1:'a', 2:'b', 3:'c'})
 q = tuple(range(1,6))
 r = tuple('rakesh')
-print(a, type(a))       #1,2,3,4,5 <class 'tuple'>
-print(b, type(b))       #1,2,3,4 <class 'tuple'>
-print(c, type(c))       #True,1,3j,None <class 'tuple'>
+print(a, type(a))       #(1,2,3,4,5) <class 'tuple'>
+print(b, type(b))       #(1,2,3,4) <class 'tuple'>
+print(c, type(c))       #(True,1,3j,None) <class 'tuple'>
 print(d, type(d))       #5.6 <class 'float'>
-print(e, type(e))       #1+3j <class 'complex'>
+print(e, type(e))       #(1+3j) <class 'complex'>
 print(f, type(f))       #1 <class 'int'>
 print(g, type(g))       #True <class 'bool'>
-print(h, type(h))
-print(i, type(i))
-print(j, type(j))
-print(k, type(k))
-print(type(l))
+print(h, type(h))       #3j <class 'complex'>
+print(i, type(i))       #(1,) <class 'Tuple'>
+print(j, type(j))       #(True,) <Class 'Tuple'>
+print(k, type(k))       #(3j,)n<class 'Tuple'>
+print(type(l))          #<class 'Tuple'>
 #print(m) 
-print(n)
-print(o)
-print(p)
-print(q)
-print(r)
+print(n)                #(2,3,4)
+print(o)                #(4,5,6)
+print(p)                #(1,2,3)
+print(q)                #(1,2,3,4,5)
+print(r)                #('r','a','k','e','s','h')
 
 # #Set 
-# a = {}
-# b = set() 
-# c = {1,2,3,4}
-# d = {1,1,2,2,2,3,3,3,4,4,4,4}
-# e = {[1,2,3], 4, True}
-# f = {{1,2,3}, 4, True}
-# g = {{1:'a', 2:'b'}, 4, True}
-# h = {(1,2,3), 4, True} 
-# i = {'rakesh', 4, True}
-# j = set(1,2,2,3,3,4,4,4)
-# k = set([1,2,2,3,3,3])
-# l = set((4,4,5,5,6,6))
-# m = set({1:'a', 1:'b', 1:'c'})
-# n = set('rraakkeesshh')
-# o = set(range(1,6))
-# print(a, type(a))
-# print(b, type(b))
-# print(c)
-# print(d)
-# print(e)
-# print(f)
-# print(g)
-# print(h)
-# print(i)
-# print(j) 
-# print(k)
-# print(l)
-# print(m)
-# print(n) 
-# print(o)
+a = {}
+b = set() 
+c = {1,2,3,4}
+d = {1,1,2,2,2,3,3,3,4,4,4,4}
+#e = {[1,2,3], 4, True}
+#f = {{1,2,3}, 4, True}
+#g = {{1:'a', 2:'b'}, 4, True}
+h = {(1,2,3), 4, True} 
+i = {'rakesh', 4, True}
+#j = set(1,2,2,3,3,4,4,4)
+k = set([1,2,2,3,3,3])
+l = set((4,4,5,5,6,6))
+m = set({1:'a', 1:'b', 1:'c'})
+n = set('rraakkeesshh')
+o = set(range(1,6))
+print(a, type(a))         #{} <class 'dict'>
+print(b, type(b))         #set() <class 'set'>
+print(c)                  #{1,2,3,4}
+print(d)                  #{1,2,3,4}
+#print(e)                  #error
+#print(f)                  #error
+#print(g)                  #error
+print(h)                  #{(1,2,3), 4, True}
+print(i)                  #{'rakesh', 4, True}
+#print(j)                  #error
+print(k)                  #{1,2,3}
+print(l)                  #{4,5,6}
+print(m)                  #{1}
+print(n)                  #
+print(o)                  #{1,2,3,4,5}
 
 # #Dict 
-# a = {}
-# b = dict() 
-# c = {1,2,3,4,5}
-# d = {1:'a', 2:'b', 3:'c', 4:'d'}
-# e = {[1,2,3]:'a', 2:'b'}
-# f = {{1,2,3}:'a', 2:'b'}
-# g = {'rakesh':'a', 2:'b'}
-# h = {(1,2,3):'a', 2:'b'}
-# i = {1:'a', 1:'b', 1:'c', 2:'x', 2:'y'}
-# j = dict(1,2,3,4,5)
-# # k = dict(1:'a', 2:'b')
-# l = dict({1:'a', 2:'b'})
-# m = dict([(1,2), [3,4], (5,6)])
-# n = dict( ((1,2),[3,4])) 
-# print(a, type(a))
-# print(b)
-# print(c)
-# print(d)
-# print(e)
-# print(f)
-# print(g)
-# print(h)
-# print(i)
-# print(j)
-# print(k)
-# print(l)
-# print(m)
-# print(n)
+a = {}
+b = dict() 
+c = {1,2,3,4,5}
+d = {1:'a', 2:'b', 3:'c', 4:'d'}
+#e = {[1,2,3]:'a', 2:'b'}
+#f = {{1,2,3}:'a', 2:'b'}
+g = {'rakesh':'a', 2:'b'}
+h = {(1,2,3):'a', 2:'b'}
+i = {1:'a', 1:'b', 1:'c', 2:'x', 2:'y'}
+#j = dict(1,2,3,4,5)
+# k = dict(1:'a', 2:'b')
+l = dict({1:'a', 2:'b'})
+m = dict([(1,2), [3,4], (5,6)])
+n = dict( ((1,2),[3,4])) 
+print(a, type(a))  #{} <class 'dict'>
+print(b)           #{}
+print(c)           #{1,2,3,4,5}
+print(d)           #{1:'a', 2:'b', 3:'c', 4:'d'}
+#print(e)           #error
+#print(f)           #error
+print(g)           #{'rakesh':'a', 2:'b'}
+print(h)           #{(1, 2, 3): 'a', 2: 'b'}
+print(i)           #{1:'c',2:'y'}
+#print(j)           #error
+#print(k)           #error
+print(l)           #{1:'a', 2:'b'}
+print(m)           #{1:2,3:4,5:6}
+print(n)           #{1:2,3:4}
 
 # #String
-# a = 'rakesh'
-# b = "rakesh"
-# c = '''r
-# a
-# k
-# esh'''
-# print(a)
-# print(type(a))
-# print(b)
-# print(type(b))
-# print(c)
-# print(type(c)) 
+a = 'rakesh'
+b = "rakesh"
+c = '''r
+a
+k
+esh'''
+print(a)  #rakesh
+print(type(a)) #<class 'str'>
+print(b)       #rakesh
+print(type(b)) #<class 'str'>
+print(c)       #r  a  k   esh"
+print(type(c)) #class 'str'>
 
 # #Range
-# a = range(5)
-# b = range(3,7)
-# c = range(3, 9, 2)
-# d = range(9,3,-1)
-# print(a)
-# print(*a)
-# print(*b)
-# print(*c)
-# print(*d)
+a = range(5)
+b = range(3,7)
+c = range(3, 9, 2)
+d = range(9,3,-1)
+print(a)   #0,1,2,3,4
+print(*a)  
+print(*b)
+print(*c)
+print(*d)
 
 # #Slicing
-# a = [4,1,2,3,5] 
-# print(a[:])
-# print(a[:3])
-# print(a[2:])
-# print(a[::-1])
-# print(a[:3:-1])
-# print(a[3::-1])
-# b = {3,2,4,6}
-# print(b[:3])
-# c = {1:'a', 2:'b', 3:'c'}
-# print(c[:2])
+a = [4,1,2,3,5] 
+print(a[:])        #4,1,2,3,5
+print(a[:3])       #4,1,2
+print(a[2:])       #2,3,5
+print(a[::-1])     #5,3,2,1,4
+print(a[:3:-1])    #5
+print(a[3::-1])    #3,2,1,4
+#b = {3,2,4,6}
+#print(b[:3])       #3,2,4
+#c = {1:'a', 2:'b', 3:'c'}
+#print(c[:2])   
